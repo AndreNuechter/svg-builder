6de13f7890cb0e309c10baef1697a81c730d3dbb
AN <andre_nuechter@hotmail.de>
2021-12-20 16:44:39 +0100

    Tweak tabs to stop breaking browser history

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,27 +1,22 @@
 export default (() => {
-    const { body } = document;
-    const tabsContainer = document.getElementById('tabs');
-    const tabs = [...tabsContainer.children];
-    const tabNames = ['drawing', 'output'];
-    const setOrRestoreTab = () => {
-        const hash = window.location.hash.slice(1);
-        const tabName = tabNames.includes(hash) ? hash : tabNames[0];
+    const tabLinksContainer = document.getElementById('tab-links');
+    const tabLinks = [...tabLinksContainer.children];
+    const tabs = [{
+        id: 'drawing',
+        element: document.querySelector('[data-tab="drawing"]'),
+    }, {
+        id: 'output',
+        element: document.querySelector('[data-tab="output"]'),
+    }];
 
-        body.dataset.activeTab = tabName;
-        document.querySelector(`a[data-tab-name="${tabName}"]`).click();
+    tabLinksContainer.onclick = ({ target }) => {
+        if (!target.classList.contains('tab-link')) return;
 
-        if (tabName !== hash) {
-            window.location.hash = tabName;
-        }
-    };
-
-    window.addEventListener('DOMContentLoaded', setOrRestoreTab, { once: true });
-    window.onhashchange = setOrRestoreTab;
-    tabsContainer.onclick = ({ target }) => {
-        if (!target.classList.contains('tab')) return;
-
-        body.dataset.activeTab = target.dataset.tabName;
-        tabs.forEach((t) => t.classList.remove('active'));
+        // destyle all links, then style the one clicked
+        tabLinks.forEach((t) => t.classList.remove('active'));
         target.classList.add('active');
+        // hide all tabs, then show the one matching the clicked tab-link
+        tabs.forEach((tab) => tab.element.classList.remove('active'));
+        tabs.find(({ id }) => id === target.dataset.tabName).element.classList.add('active');
     };
 })();
