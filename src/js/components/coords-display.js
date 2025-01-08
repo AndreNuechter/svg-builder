ca365a1906c49ae396818c8bf95ed89ce0ac352a
Andre Nuechter <andre_nuechter@hotmail.de>
2025-01-08 14:42:26 +0100

    Slightly refactor coords display

--- src/js/components/coords-display.js
+++ src/js/components/coords-display.js
@@ -2,17 +2,20 @@ import { getSVGCoords } from '../helper-functions.js';
 import { svg } from '../dom-shared-elements.js';
 
 const coords = document.getElementById('coords');
-const coordToolTips = (event) => {
+
+export default (() => {
+    svg.addEventListener('pointermove', showSVGCoordsInTooltip);
+    svg.addEventListener('pointerleave', resetCoordsDisplay);
+})();
+
+function showSVGCoordsInTooltip(event) {
     const [x, y] = getSVGCoords(event);
 
     coords.textContent = `x: ${Math.trunc(x)}, y: ${Math.trunc(y)}`;
-    coords.style.left = `${event.pageX - 120}px`;
-    coords.style.top = `${event.pageY - 40}px`;
-};
+    coords.style.left = `${event.x - 120}px`;
+    coords.style.top = `${event.y - 40}px`;
+}
 
-export default (() => {
-    svg.addEventListener('pointermove', coordToolTips);
-    svg.addEventListener('pointerleave', () => {
-        coords.style = null;
-    });
-})();
+function resetCoordsDisplay() {
+    coords.style = null;
+}
